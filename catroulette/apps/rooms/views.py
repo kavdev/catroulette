93363f4bce87cd9cae2c37caa295e82d62a77e36
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-03-15 21:49:36 -0700

    better error checking in get_room method

--- catroulette/apps/rooms/views.py
+++ catroulette/apps/rooms/views.py
@@ -7,6 +7,7 @@
 """
 
 from django.contrib.auth import get_user_model as get_cat_model
+from django.http.response import HttpResponseBadRequest
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
@@ -21,14 +22,17 @@ from .utils import match_cat
 def get_room(request):
     """
     Cat posted personality details. Create a cat object in the
-    database with those details and attempt to find a match
+    database with those details and attempt to find a match.
     """
 
-    vocalness = request.POST["vocalness"]
-    intelligence = request.POST["intelligence"]
-    energy = request.POST["energy"]
+    try:
+        vocalness = int(request.POST.get("vocalness"))
+        intelligence = int(request.POST.get("intelligence"))
+        energy = int(request.POST.get("energy"))
+    except TypeError:
+        return HttpResponseBadRequest()
+    else:
+        new_cat = get_cat_model()(vocalness=vocalness, intelligence=intelligence, energy=energy)
+        new_cat.save()
 
-    new_cat = get_cat_model()(vocalness=vocalness, intelligence=intelligence, energy=energy)
-    new_cat.save()
-
-    return {"room_name": match_cat(new_cat)}
+        return {"room_name": match_cat(new_cat)}
